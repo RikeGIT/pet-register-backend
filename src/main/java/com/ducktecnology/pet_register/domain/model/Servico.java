ba7fe6fe6dc4bfdee60e07ad5095facbaa0609e0
rike <rikecristian@gmail.com>
2026-05-20 14:36:29 -0300

    update

--- src/main/java/com/ducktecnology/pet_register/domain/model/Servico.java
+++ src/main/java/com/ducktecnology/pet_register/domain/model/Servico.java
@@ -10,5 +10,7 @@ public class Servico {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private String descricao;
+    private Double preco;
     private Integer duracaoMinutos;
 }
